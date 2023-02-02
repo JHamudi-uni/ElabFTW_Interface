85dc1a0a08d3fe2cde9f2996537a355e394ec6e5
JHamudi <76528081+JHamudi@users.noreply.github.com>
2023-02-02 14:30:52 +0100

    update

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import elabapy
+# json is used for encoding and decoding JSON data.
 import json
+# imports the HTTPError exception from the requests library, which is used to handle errors that occur
+# during HTTP requests.
 from requests.exceptions import HTTPError
-# initialize the manager with an endpoint and your token
-manager = elabapy.Manager(endpoint="https://elab.example.org/api/v1/", token="3148")
-# get experiment with id 42
+
+
+# creates an instance of the Manager class from the elabapy library, initializing it with the endpoint and API token
+manager = elabapy.Manager(endpoint="https://demo.elabftw.net/api/v1/", token="1ddbf1d38905d0b8d48c8992783a50320d0103fb19c9079259b9defbeb4bc11447da883ac6facbff1152210")
+params = { "tag": "some-tag"}
+
+# get experiment with id 13170
 try:
-    exp = manager.get_experiment(42)
+    exp = manager.get_experiment(13170)
+
+    # add tag "some-tag" to experiment 13170
+    print(manager.add_tag_to_experiment(13170, params))
+
+    # uses the json.dumps function to convert the exp variable (which contains information about an experiment)
+    # into a string representation in JSON format. The indent parameter is set to 4 to make the JSON output more
+    # readable, and the sort_keys parameter is set to True to sort the keys in the JSON output.
     print(json.dumps(exp, indent=4, sort_keys=True))
+
 # if something goes wrong, the corresponding HTTPError will be raised
 except HTTPError as e:
     print(e)
+    
